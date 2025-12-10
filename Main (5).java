54d6e6c63ab4e503bd5a84d72973aaa7e9e38bd3
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-10 19:01:12 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double num=sc.nextDouble();
+     System.out.printf("%.2f",num);   
+    }
+}
