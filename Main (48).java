1227826c73a7fa35bb0ceb86991f9e8137814d76
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 16:17:44 +0530

    Add files via upload

---
+++ Main (48).java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	   System.out.println("Kilometer: ");
+	   int k=sc.nextInt();
+	   int result=k*1000;
+	   System.out.println("Kilometers to meters: "+result);
+}
+}
